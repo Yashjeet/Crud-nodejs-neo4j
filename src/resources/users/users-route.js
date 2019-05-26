e9d5f4c6ab8535dfc9b9f5a10088751e5fbfe6db
Yashjeet Luthra <luthra.yashjeet@gmail.com>
2019-05-27 03:38:52 +0530

    fix routes

--- src/resources/users/users-route.js
+++ src/resources/users/users-route.js
@@ -6,7 +6,7 @@ const UpdateUserApi = require('./apis/update-user-api');
 const GetUserApi = require('./apis/get-user-api');
 const DeleteUserApi = require('./apis/delete-user-api');
 
-router.post('/create', CreateUserApi.post);
+router.post('', CreateUserApi.post);
 router.put('/:userId', UpdateUserApi.update);
 router.get('/:userId', GetUserApi.get);
 router.delete('/:userId', DeleteUserApi.delete);
